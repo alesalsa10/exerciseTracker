e83c4af3bfce704ba18c9eede946159716ace211
alesalsa10 <alexandersalsa10@gmail.com>
2021-04-10 12:02:01 -0400

    working on user registration

--- Server.js
+++ Server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const connectToDb = require('./configs/database');
 require('dotenv').config();
-const User = require('./models/User')
+const cors = require('cors')
 
 const app = express();
+app.use(express.json())
+app.use(cors);
 const port = process.env.PORT || 5000;
 
 connectToDb()
 
+//Routes
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
+app.use('/register', require('./routes/register'))
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port} `);
---
+++ routes/register.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+var createError = require('http-errors');
+const bcrypt = require('bcrypt');
+
+
+const User = require('../models/User');
+
+
+
+router.post(
+  '/',
+  check('email').isEmail().withMessage('Must be an email'),
+  check('password').isLength({min: 8}).withMessage('Password must be at least 8 characters long'),
+  check('username').not().isEmpty().withMessage('Name is required').isLength({max: 20}).withMessage('Username must be less than 20 characters'),
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const {username, email, password} = req.body;
+
+      try{
+        let user = await User.findOne({email});
+
+        //if user exists return error
+        if(user){
+            return createError(400, 'User already exists')
+        }
+
+        //create user
+        user = new User({
+            username, email, password
+        })
+
+        //encrypt password
+        //const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, 10);
+
+        //save user
+        await user.save()
+
+      }catch(error){
+        console.log(error);
+        return createError(500, 'Something went wrong')
+      }
+  }
+);
+
+module.exports = router;
